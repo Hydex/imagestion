fbf25e4c03f36e55e71d9f027848496b4d6d1ac2
miguelote <miguelote@gmail.com>
2010-11-08 01:01:10 +0000

    - optimizacion de dilate y erode para multithreads

--- imagestion1.0/src/test.py
+++ imagestion1.0/src/test.py
@@ -36,30 +36,27 @@
 ## http://docs.python.org/reference/index.html
 from Imagen import *
 from datetime import datetime
-#from array import *
-#import thread
 
 ## http://luispedro.org/pymorph-apidocs/html/genindex.html
-#from pymorph import *
 
 ## REALIZAR CAMBIOS EN: 
 ## sudo gedit /usr/lib/python2.6/dist-packages/PIL/Image.py
 ## http://hg.effbot.org/pil-2009-raclette/changeset/fb7ce579f5f9
 ##
-##1494    def split(self):
-##1495        "Split image into bands"
-##1496
-##1497        self.load()
-##1498        if self.im.bands == 1:
-##1499            ims = [self.copy()]
-##1500        else:
-##1501            ims = []
-##1502            for i in range(self.im.bands):
-##1503                ims.append(self._new(self.im.getband(i)))
-##1504        return tuple(ims)
+# 1494    def split(self):
+# 1495        "Split image into bands"
+# 1496
+# 1497        self.load()
+# 1498        if self.im.bands == 1:
+# 1499            ims = [self.copy()]
+# 1500        else:
+# 1501            ims = []
+# 1502            for i in range(self.im.bands):
+# 1503                ims.append(self._new(self.im.getband(i)))
+# 1504        return tuple(ims)
 
 
-img = Imagen('../../../webcam/miguelote.jpg')
+img = Imagen('../../../webcam/img01.jpg')
 print 'ancho:', img.getAncho() ,' alto:', img.getAlto()
 
 #r = img.getR()
@@ -82,6 +79,7 @@ img.getRGB().show()
 
 start = datetime.now()
 
+#img.dilate()
 img.erode()
 
 stop = datetime.now()
