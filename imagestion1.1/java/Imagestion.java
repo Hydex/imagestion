1956da4dc74d137aab50389653e6b87d2b7ee7d3
miguelote <miguelote@gmail.com>
2010-12-06 02:40:44 +0000

    implementacion de elemento estructurante en operacion dilate

--- imagestion1.1/java/Imagestion.java
+++ imagestion1.1/java/Imagestion.java
@@ -55,12 +55,15 @@ public class Imagestion {
 
             try
             {
+                Integer[][] elementoEstructurante = {{0,1,1,0},{1,1,1,1},{1,1,1,1},{0,1,1,0}};
+                
                 Imagen img = new Imagen(file);
+                img.setElementoEstructurante(4, 4, elementoEstructurante);
                 //img.debug = true;
 //                img.quickDilate();
 //                img.quickDilate();
-                img.dilate();
-                img.dilate();
+//                img.dilate();
+//                img.dilate();
                 img.dilate();
                 img.guardar(file2);
 
