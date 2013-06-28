3d2784121e71a2407c8e9d9880e0de8c1962b099
miguelwelch <miguelwelch@958553ba-40fc-a038-aa7d-849bcbbf9f75>
2013-06-28 19:04:08 +0000

    debugging

--- imagestion1.0/src/test2.py
+++ imagestion1.0/src/test2.py
@@ -1,26 +1,24 @@
 from Red import *
 import json
 
-net = Red(2,2,[2,3,2],['LOGSIG','LOGSIG','LOGSIG'])
+net = Red(2,1,[2,3,1],['LOGSIG','LOGSIG','LOGSIG'])
 
 #print net.getConfiguracion()
 
-for i in range(len(net.capas)):
-    for j in range(len(net.capas[i])):
-        #print "capas["+str(i)+"]["+str(j)+"]entradas:"+json.dumps(net.capas[i][j].entradas, sort_keys=True,indent=4, separators=(',', ': '))
-        print "capas["+str(i)+"]["+str(j)+"]."+net.capas[i][j].name+".pesos   :"+json.dumps(net.capas[i][j].pesos, sort_keys=True,indent=4, separators=(',', ': '))
-#print net.capas
+print "PESOS"
+print net.getPesos()
+#print "ENTRADAS"
+#print net.getEntradas()
+#print "SIMULAR"
+#print net.simular([0.5,0.6])
 
-print "SIMULAR"
-print net.simular([0.5,0.6])
-
-print "sinapsis:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
+#print "sinapsis:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
 
 print "ENTRENAR"
 net.entrenar([
-        [0.0,0.0], [0.0,1.0], [1.0,0.0], [1.0,1.0]
+        [0.1,0.1], [0.1,0.9], [0.9,0.1], [0.9,0.9]
     ],[
-        [0.0,0.0], [1.0,1.0], [1.0,1.0], [0.0,0.0]
+        [0.0], [1.0], [1.0], [0.0]
     ])
 
 print "sinapsis:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
