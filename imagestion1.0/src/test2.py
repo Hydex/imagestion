9d878df1ba34e21e08c5c8d2196764f8b8737d0c
miguelwelch <miguelwelch@958553ba-40fc-a038-aa7d-849bcbbf9f75>
2013-09-09 22:07:46 +0000

    correcciones en calculo de error y delta

--- imagestion1.0/src/test2.py
+++ imagestion1.0/src/test2.py
@@ -13,31 +13,35 @@ net = Red(2,1,[2,1],['LOGSIG','LOGSIG'])
 #    for j in range(len(net.capas[i])):
 #        #print "capas["+str(i)+"]["+str(j)+"]entradas:"+json.dumps(net.capas[i][j].entradas, sort_keys=True,indent=4, separators=(',', ': '))
 #        print "capas["+str(i)+"]["+str(j)+"]."+net.capas[i][j].name+".pesos   :"+json.dumps(net.capas[i][j].pesos, sort_keys=True,indent=4, separators=(',', ': '))
-print 'PESOS: '+str(net.getPesos())
-print 'DELTAS: '+str(net.getDeltas())
+print 'PESOS  : '+str(net.getPesos())
+print 'DELTAS : '+str(net.getDeltas())
+print 'ERRORES: '+str(net.getErrores())
 
-##print "SIMULAR"
-##print net.simular([0.0,1.0])
-##print "sinapsis:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
+print "0 SIMULAR"
+print str([0,0]) + ' => ' + str(net.simular([O,O]))
+print str([0,1]) + ' => ' + str(net.simular([O,I]))
+print str([1,0]) + ' => ' + str(net.simular([I,O]))
+print str([1,1]) + ' => ' + str(net.simular([I,I]))
 
 for x in range(1):
-    print str(x)+" ENTRENAR"
+    #print str(x+1)+" ENTRENAR"
     #net.setEpochs(100)
     net.entrenar([
             #[0.0001,0.0001], [0.0001,1.0], [1.0,0.0001], [1.0,1.0]
-            [I,I], [I,O], [O,I], [O,O]
+            [O,O], [O,I], [I,O], [I,I]
         ],[
             [0], [1], [1], [0]
         ])
 
-    print str(x)+" SIMULAR"
+    print str(x+1)+" SIMULAR"
     print str([0,0]) + ' => ' + str(net.simular([O,O]))
     print str([0,1]) + ' => ' + str(net.simular([O,I]))
     print str([1,0]) + ' => ' + str(net.simular([I,O]))
     print str([1,1]) + ' => ' + str(net.simular([I,I]))
 
-    print 'PESOS: '+str(net.getPesos())
-    print 'DELTAS: '+str(net.getDeltas())
+    print 'PESOS  : '+str(net.getPesos())
+    print 'DELTAS : '+str(net.getDeltas())
+    print 'ERRORES: '+str(net.getErrores())
 
 #print "SINAPSIS:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
 net.panic = True
