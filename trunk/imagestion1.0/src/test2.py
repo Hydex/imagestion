947b416fd19989298139f2b01d902aaeef7660dc
miguelwelch <miguelwelch@958553ba-40fc-a038-aa7d-849bcbbf9f75>
2013-10-29 18:13:47 +0000

    reconstruccion de entrenamiento

--- trunk/imagestion1.0/src/test2.py
+++ trunk/imagestion1.0/src/test2.py
@@ -1,15 +1,14 @@
 from Red import *
 import json
 
-O = 0.1 #0.000001
-I = 1.1 #0.999999
+O = 0.000001
+I = 0.999999
 
 #net = Net(2,1,[2,1],['TANSIG','TANSIG'])
 net = Net(2,1,[2,1],['LOGSIG','LOGSIG'])
 
 print 'PESOS  : '+str(net.getPesos())
 print 'DELTAS : '+str(net.getDeltas())
-print 'ERRORES: '+str(net.getErrores())
 
 print "0 SIMULAR"
 print str([O,O]) + ' => ' + str(net.simular([O,O]))
@@ -19,7 +18,7 @@ print str([I,I]) + ' => ' + str(net.simular([I,I]))
 
 for x in range(1):
     print str(x+1)+" ENTRENAR"
-    #net.setEpochs(15)
+    net.setEpochs(100)
     net.entrenar([
             #[0.0001,0.0001], [0.0001,1.0], [1.0,0.0001], [1.0,1.0]
             [O,O], [O,I], [I,O], [I,I]
@@ -35,10 +34,9 @@ for x in range(1):
 
     print 'PESOS  : '+str(net.getPesos())
     print 'DELTAS : '+str(net.getDeltas())
-    print 'ERRORES: '+str(net.getErrores())
 
 #print "SINAPSIS:"+json.dumps(net.sinapsis, sort_keys=True,indent=4, separators=(',', ': '))
-net.panic = True
+#net.panic = True
 print net.getHistorial()
 
 if net.panic:
