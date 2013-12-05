e0cb9a5230e491a94e20c1e016d9853aecafb132
miguelwelch <miguelwelch@958553ba-40fc-a038-aa7d-849bcbbf9f75>
2013-12-05 21:52:45 +0000

    reconstruccion backpropagation

--- trunk/imagestion1.0/src/test2.py
+++ trunk/imagestion1.0/src/test2.py
@@ -1,11 +1,11 @@
 from Red import *
 import json
 
-O = 0.1 #0.000001
-I = 0.9 #0.999999
+O = 0.01 #0.000001
+I = 0.99 #0.999999
 
-net = Net(2,1,[2,1],['TANSIG','TANSIG'])
-#net = Net(2,1,[2,1],['LOGSIG','LOGSIG'])
+#net = Net(2,1,[3,2,1],['TANSIG','TANSIG','TANSIG'])
+net = Net(2,1,[3,2,1],['LOGSIG','LOGSIG','LOGSIG'])
 
 print 'PESOS : '+str(net.getPesos())
 print 'DELTA : '+str(net.getDeltas())
