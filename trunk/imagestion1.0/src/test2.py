3024ea400840ad1b3fcc135595f254eb6d03b71a
miguelwelch <miguelwelch@958553ba-40fc-a038-aa7d-849bcbbf9f75>
2014-06-18 21:01:41 +0000

    implementacion de getConfiguracion para red

--- trunk/imagestion1.0/src/test2.py
+++ trunk/imagestion1.0/src/test2.py
@@ -7,7 +7,7 @@ I = 1 #0.99 #0.999999
 # Net(entradas,salidas,[nodos,...],[funciones])
 #net = Net(2,1,[2,1],['TANSIG','TANSIG'])
 net = Net(2,1,[2,2,1],['LOGSIG','LOGSIG','LOGSIG'])
-
+print 'NET   : Net(2,1,[2,1],[LOGSIG,LOGSIG])'
 print 'PESOS : '+str(net.getPesos())
 print 'DELTA : '+str(net.getDeltas())
 print 'ERROR : '+str(net.getErrores())
@@ -18,7 +18,7 @@ print str([O,I]) + ' => ' + str(net.simular([O,I]))
 print str([I,O]) + ' => ' + str(net.simular([I,O]))
 print str([I,I]) + ' => ' + str(net.simular([I,I]))
 
-for x in range(1):
+for x in range(2):
     print str(x+1)+" ENTRENAR"
     net.setEpochs(100)
     net.entrenar([
@@ -42,11 +42,14 @@ for x in range(1):
 #net.panic = True
 #print net.getHistorial()
 
+print ""
+print "print CONFIGURACION"
+print ""
+print dumps(net.getConfiguracion(), sort_keys=True,indent=4, separators=(',', ': '))
+
 if net.panic:
     print "print LOG"
     print net.printLog()
-    print "print CONFIGURACION"
-    print str(net)
     
 
 
